5d42bb915c43501d67cef3e851d7ea1f85a0595e
chung-ming <chungming_tsen@outlook.com>
2021-04-29 20:48:10 +0800

    Fail test case

---
+++ AspNetCoreWebApplicationTest/Controllers/HelloControllerTest.cs
@@ -0,0 +1,17 @@
+using AspNetCoreWebApplication.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApplicationTest.Controllers
+{
+    public class HelloControllerTest
+    {
+        [Fact]
+        public void NoInputParamGetResponseTest()
+        {
+            HelloController controller = new HelloController();
+            var response = controller.Get().Value as Response;
+            Assert.Equal("Hello World!!!", response.output);
+        }
+    }
+}
